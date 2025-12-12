967682cac85a0c9ba224ba4a99340bae6dfe60fc
leon <a0932936319@gmail.com>
2025-12-12 15:51:33 +0800

    fix: testcontainer config

--- src/test/java/com/leon/springbootleon/TestcontainersConfiguration.java
+++ src/test/java/com/leon/springbootleon/TestcontainersConfiguration.java
@@ -1,11 +1,10 @@
 package com.leon.springbootleon;
 
-import lombok.SneakyThrows;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @ActiveProfiles("test")
@@ -14,8 +13,9 @@ public class TestcontainersConfiguration {
 
     @Bean
     @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17.4"))
+    @SuppressWarnings("resource")
+    PostgreSQLContainer postgresContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:17.4"))
                 .withInitScript("db/init-db.sql");
     }
 
